5ae6d95ccfae4da387aa3046b764c2a91d00047c
pachpolkalyani <89258306+pachpolkalyani@users.noreply.github.com>
2021-08-27 14:31:25 +0530

    Update main.tf

--- TF/main.tf
+++ TF/main.tf
@@ -30,4 +30,13 @@ resource "azurerm_storage_account" "STG" {
    account_replication_type = var.var_account_replication_type
  }
 
+module Modules {
+  Source = "Modules"
+  name = var.var_Storage_name
+   resource_group_name = var.var_RG_name
+   location = var.var_location
+   account_tier = var.var_account_tier
+   account_replication_type = var.var_account_replication_type
+  }
+  
 
